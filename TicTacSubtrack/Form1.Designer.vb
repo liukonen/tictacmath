a8cf1535ec4b42f93df41d062aeebfcd2135a2b6
Luke Liukonen <mfanzone@hotmail.com>
2016-02-20 09:43:54 -0600

    Inital Checkin

---
+++ TicTacSubtrack/Form1.Designer.vb
@@ -0,0 +1,412 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        Me.Panel8.SuspendLayout()
+        Me.Panel9.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        resources.ApplyResources(Me.Panel1, "Panel1")
+        Me.Panel1.Name = "Panel1"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.TextBox2)
+        Me.Panel2.Controls.Add(Me.Label2)
+        resources.ApplyResources(Me.Panel2, "Panel2")
+        Me.Panel2.Name = "Panel2"
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        resources.ApplyResources(Me.TextBox2, "TextBox2")
+        Me.TextBox2.Name = "TextBox2"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.Button3)
+        Me.Panel3.Controls.Add(Me.TextBox3)
+        Me.Panel3.Controls.Add(Me.Label3)
+        resources.ApplyResources(Me.Panel3, "Panel3")
+        Me.Panel3.Name = "Panel3"
+        '
+        'Button3
+        '
+        resources.ApplyResources(Me.Button3, "Button3")
+        Me.Button3.Name = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        resources.ApplyResources(Me.TextBox3, "TextBox3")
+        Me.TextBox3.Name = "TextBox3"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Panel4
+        '
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Controls.Add(Me.Button4)
+        Me.Panel4.Controls.Add(Me.TextBox4)
+        Me.Panel4.Controls.Add(Me.Label4)
+        resources.ApplyResources(Me.Panel4, "Panel4")
+        Me.Panel4.Name = "Panel4"
+        '
+        'Button4
+        '
+        resources.ApplyResources(Me.Button4, "Button4")
+        Me.Button4.Name = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        resources.ApplyResources(Me.TextBox4, "TextBox4")
+        Me.TextBox4.Name = "TextBox4"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Panel5
+        '
+        Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel5.Controls.Add(Me.Button5)
+        Me.Panel5.Controls.Add(Me.TextBox5)
+        Me.Panel5.Controls.Add(Me.Label5)
+        resources.ApplyResources(Me.Panel5, "Panel5")
+        Me.Panel5.Name = "Panel5"
+        '
+        'Button5
+        '
+        resources.ApplyResources(Me.Button5, "Button5")
+        Me.Button5.Name = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        resources.ApplyResources(Me.TextBox5, "TextBox5")
+        Me.TextBox5.Name = "TextBox5"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'Panel6
+        '
+        Me.Panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel6.Controls.Add(Me.Button6)
+        Me.Panel6.Controls.Add(Me.TextBox6)
+        Me.Panel6.Controls.Add(Me.Label6)
+        resources.ApplyResources(Me.Panel6, "Panel6")
+        Me.Panel6.Name = "Panel6"
+        '
+        'Button6
+        '
+        resources.ApplyResources(Me.Button6, "Button6")
+        Me.Button6.Name = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        resources.ApplyResources(Me.TextBox6, "TextBox6")
+        Me.TextBox6.Name = "TextBox6"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'Panel7
+        '
+        Me.Panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel7.Controls.Add(Me.Button7)
+        Me.Panel7.Controls.Add(Me.TextBox7)
+        Me.Panel7.Controls.Add(Me.Label7)
+        resources.ApplyResources(Me.Panel7, "Panel7")
+        Me.Panel7.Name = "Panel7"
+        '
+        'Button7
+        '
+        resources.ApplyResources(Me.Button7, "Button7")
+        Me.Button7.Name = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'TextBox7
+        '
+        resources.ApplyResources(Me.TextBox7, "TextBox7")
+        Me.TextBox7.Name = "TextBox7"
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'Panel8
+        '
+        Me.Panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel8.Controls.Add(Me.Button8)
+        Me.Panel8.Controls.Add(Me.TextBox8)
+        Me.Panel8.Controls.Add(Me.Label8)
+        resources.ApplyResources(Me.Panel8, "Panel8")
+        Me.Panel8.Name = "Panel8"
+        '
+        'Button8
+        '
+        resources.ApplyResources(Me.Button8, "Button8")
+        Me.Button8.Name = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'TextBox8
+        '
+        resources.ApplyResources(Me.TextBox8, "TextBox8")
+        Me.TextBox8.Name = "TextBox8"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'Panel9
+        '
+        Me.Panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel9.Controls.Add(Me.Button9)
+        Me.Panel9.Controls.Add(Me.TextBox9)
+        Me.Panel9.Controls.Add(Me.Label9)
+        resources.ApplyResources(Me.Panel9, "Panel9")
+        Me.Panel9.Name = "Panel9"
+        '
+        'Button9
+        '
+        resources.ApplyResources(Me.Button9, "Button9")
+        Me.Button9.Name = "Button9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'TextBox9
+        '
+        resources.ApplyResources(Me.TextBox9, "TextBox9")
+        Me.TextBox9.Name = "TextBox9"
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'RadioButton1
+        '
+        resources.ApplyResources(Me.RadioButton1, "RadioButton1")
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        resources.ApplyResources(Me.RadioButton2, "RadioButton2")
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        resources.ApplyResources(Me.RadioButton3, "RadioButton3")
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        resources.ApplyResources(Me.CheckBox1, "CheckBox1")
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Panel9)
+        Me.Controls.Add(Me.Panel8)
+        Me.Controls.Add(Me.Panel7)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel6.PerformLayout()
+        Me.Panel7.ResumeLayout(False)
+        Me.Panel7.PerformLayout()
+        Me.Panel8.ResumeLayout(False)
+        Me.Panel8.PerformLayout()
+        Me.Panel9.ResumeLayout(False)
+        Me.Panel9.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+End Class
---
+++ TicTacSubtrack/Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        voice.SelectVoiceByHints(Speech.Synthesis.VoiceGender.Female)
+        voice.SpeakAsync("Hello, I am ready to play some Tic Tac Math with you today. You start first.")
+        RadioButton1.Checked = True
+        LabelArray.AddRange({Label1, Label2, Label3, Label4, Label5, Label6, Label7, Label8, Label9})
+        TextboxArray.AddRange({TextBox1, TextBox2, TextBox3, TextBox4, TextBox5, TextBox6, TextBox7, TextBox8, TextBox9})
+        ButtonArray.AddRange({Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9})
+        PopulateField()
+    End Sub
+
+    Private voice As New System.Speech.Synthesis.SpeechSynthesizer()
+    Private LabelArray As New Generic.List(Of Label)()
+    Private TextboxArray As New Generic.List(Of TextBox)()
+    Private ButtonArray As New Generic.List(Of Button)()
+    Private Sub PopulateField()
+        ComputerValues.Clear()
+        Player1Values.Clear()
+        Dim allowNegatives As Boolean = CheckBox1.Checked
+        For Each t In TextboxArray
+            If t IsNot Nothing Then
+                t.Text = String.Empty
+                t.Visible = True
+            End If
+        Next
+        Dim rand As New Random()
+
+        Dim Max As Integer = Convert.ToInt32(Math.Pow(10, RadioNumberSelected())) - 1
+        For Each L In LabelArray
+            L.ForeColor = Color.Black
+            Dim Number1 As Integer = rand.Next(1, Max)
+            Dim Number2 As Integer = rand.Next(1, Max)
+            If Not allowNegatives AndAlso Number1 < Number2 Then
+                L.Text = String.Concat(Number2.ToString, Environment.NewLine, "-", Number1.ToString)
+            Else
+                L.Text = String.Concat(Number1.ToString, Environment.NewLine, "-", Number2.ToString)
+            End If
+            L.Visible = True
+        Next
+        Button1.Visible = True : Button2.Visible = True : Button3.Visible = True : Button4.Visible = True : Button5.Visible = True : Button6.Visible = True : Button7.Visible = True : Button8.Visible = True : Button9.Visible = True
+    End Sub
+
+    Private Function RadioNumberSelected() As Integer
+        If RadioButton1.Checked Then Return 1
+        If RadioButton2.Checked Then Return 2
+        If RadioButton3.Checked Then Return 3
+        Return 0
+    End Function
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged, RadioButton3.CheckedChanged
+        If DirectCast(sender, RadioButton).Checked = True Then
+            RadioButton1.Checked = (sender Is RadioButton1)
+            RadioButton2.Checked = (sender Is RadioButton2)
+            RadioButton3.Checked = (sender Is RadioButton3)
+
+            PopulateField()
+        End If
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+        Dim ob As Button = DirectCast(sender, Button)
+        Dim intValue As Integer = ButtonArray.IndexOf(ob) + 1 'Integer.Parse(ob.Name.Substring(6))
+        Dim SelectedValue As Integer
+        'Dim LabelValueToCheck As String = LabelArray(intValue - 1).Text
+        If Not String.IsNullOrWhiteSpace(TextboxArray(intValue - 1).Text) AndAlso Integer.TryParse(TextboxArray(intValue - 1).Text, SelectedValue) Then
+            Dim Number1, Number2 As Integer
+            ParseNumbers(LabelArray(intValue - 1).Text, Number1, Number2)
+            Dim value As Integer = Number1 - Number2
+            voice.Speak(Number1.ToString & " minus " & Number2 & " equals " & value.ToString)
+            ob.Visible = False
+            If value = SelectedValue Then
+                MarkXO(True, intValue)
+            Else
+                MarkXO(False, intValue)
+            End If
+        Else
+            voice.SpeakAsync("Oops!. Enter the value in the textbox, and try again.")
+        End If
+    End Sub
+
+    Private Sub MarkXO(Player As Boolean, ArrayID As Integer)
+        Const PlayerWon As String = "You won!"
+        Const ComputerWon As String = "The computer won."
+        Dim arrayoffset As Integer = ArrayID - 1
+
+        TextboxArray(arrayoffset).Visible = False
+        If Player Then
+            Player1Values.Add(ArrayID)
+            LabelArray(arrayoffset).Text = "X"
+            LabelArray(arrayoffset).ForeColor = Color.Blue
+            If CheckForWin(Player1Values) Then
+                Win(PlayerWon)
+            Else
+                ComputersTurn()
+            End If
+        Else
+            ComputerValues.Add(ArrayID)
+            LabelArray(arrayoffset).Text = "O"
+            LabelArray(arrayoffset).ForeColor = Color.Red
+            If CheckForWin(ComputerValues) Then
+                Win(ComputerWon)
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub ComputersTurn()
+        Dim X = (From T As TextBox In TextboxArray Where T.Visible = True Select T).ToArray
+        Dim rand As New Random()
+        Dim ValueToSelect As Integer = rand.Next(0, X.Count - 1)
+        Dim I As Integer = TextboxArray.IndexOf(X(ValueToSelect))
+
+        ButtonArray(I).Visible = False
+        TextboxArray(I).Visible = False
+        Dim TextToParse As String = LabelArray(I).Text
+        Dim Number1, Number2 As Integer
+        ParseNumbers(TextToParse, Number1, Number2)
+        voice.SpeakAsync("My Pick. " & Number1.ToString & " minus " & Number2 & " equals " & (Number1 - Number2).ToString)
+        LabelArray(I).Text = "O" : LabelArray(I).ForeColor = Color.Red
+
+
+    End Sub
+
+    Private Sub ParseNumbers(S As String, ByRef Number1 As Integer, ByRef Number2 As Integer)
+        Dim Numbers() As String = S.Split(Environment.NewLine & "-")
+        Number1 = Integer.Parse(Numbers(0))
+        Number2 = Integer.Parse(Numbers(1).Replace("-", ""))
+    End Sub
+
+    Private Sub Win(TextToSpeak As String)
+        voice.SpeakAsync(TextToSpeak)
+        MsgBox(TextToSpeak)
+        PopulateField()
+    End Sub
+
+    Private Player1Values As New Generic.List(Of Integer)()
+    Private ComputerValues As New Generic.List(Of Integer)()
+
+    Private Function CheckForWin(Player As Generic.List(Of Integer)) As Boolean
+        If Player.Contains(1) AndAlso Player.Contains(2) AndAlso Player.Contains(3) Then Return True
+        If Player.Contains(1) AndAlso Player.Contains(4) AndAlso Player.Contains(7) Then Return True
+        If Player.Contains(1) AndAlso Player.Contains(5) AndAlso Player.Contains(9) Then Return True
+        If Player.Contains(2) AndAlso Player.Contains(5) AndAlso Player.Contains(8) Then Return True
+        If Player.Contains(3) AndAlso Player.Contains(6) AndAlso Player.Contains(9) Then Return True
+        If Player.Contains(3) AndAlso Player.Contains(5) AndAlso Player.Contains(7) Then Return True
+        If Player.Contains(4) AndAlso Player.Contains(5) AndAlso Player.Contains(6) Then Return True
+        If Player.Contains(7) AndAlso Player.Contains(8) AndAlso Player.Contains(9) Then Return True
+        Return False
+    End Function
+
+
+End Class
