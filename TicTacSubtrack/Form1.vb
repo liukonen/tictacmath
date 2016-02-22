1292a46637a58a8a5341a5e5ae294ade3cf0b4cb
Luke Liukonen <mfanzone@hotmail.com>
2016-02-21 20:26:31 -0600

    cleanup of code, and moved some things around.

--- TicTacSubtrack/Form1.vb
+++ TicTacSubtrack/Form1.vb
@@ -1,28 +1,80 @@
 ﻿Imports System.ComponentModel
 
 Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        voice.SelectVoiceByHints(Speech.Synthesis.VoiceGender.Female)
-        voice.SpeakAsync("Hello, I am ready to play some Tic Tac Math with you today. You start first.")
 
-        LabelArray.AddRange({Label1, Label2, Label3, Label4, Label5, Label6, Label7, Label8, Label9})
-        TextboxArray.AddRange({TextBox1, TextBox2, TextBox3, TextBox4, TextBox5, TextBox6, TextBox7, TextBox8, TextBox9})
-        ButtonArray.AddRange({Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9})
-        PopulateField()
-    End Sub
-
+#Region "Private Objects"
     Private voice As New System.Speech.Synthesis.SpeechSynthesizer()
     Private LabelArray As New Generic.List(Of Label)()
     Private TextboxArray As New Generic.List(Of TextBox)()
     Private ButtonArray As New Generic.List(Of Button)()
     Private allowNegatives As Boolean = False
     Private ActiveGameType As GameType = GameType.Subtract
+    Private RadioNumberSelected As Integer = 1
+    Private Player1Values As New Generic.List(Of Integer)()
+    Private ComputerValues As New Generic.List(Of Integer)()
+#End Region
 
+#Region "Enum Types"
     Private Enum GameType As Byte
         Add
         Subtract
         Multiply
     End Enum
+#End Region
+
+#Region "Properties"
+    Private ReadOnly Property GameOperator As Char
+        Get
+            Select Case ActiveGameType
+                Case GameType.Multiply
+                    Return "x"c
+                Case GameType.Add
+                    Return "+"c
+                Case Else
+                    Return "-"c
+            End Select
+        End Get
+    End Property
+
+    Private ReadOnly Property GetActiveTypesOperatorName As String
+        Get
+            Select Case ActiveGameType
+                Case GameType.Add
+                    Return " plus "
+                Case GameType.Multiply
+                    Return " times "
+                Case Else
+                    Return " minus "
+            End Select
+        End Get
+    End Property
+#End Region
+
+#Region "Event Handles"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+        Dim ob As Button = DirectCast(sender, Button)
+        Dim intValue As Integer = ButtonArray.IndexOf(ob) + 1
+        Dim SelectedValue As Integer
+        If Not String.IsNullOrWhiteSpace(TextboxArray(intValue - 1).Text) AndAlso Integer.TryParse(TextboxArray(intValue - 1).Text, SelectedValue) Then
+            ob.Visible = False
+
+            Dim Number1, Number2 As Integer
+            ParseNumbers(LabelArray(intValue - 1).Text, Number1, Number2)
+            Dim value As Integer = CalculateValue(Number1, Number2)
+            voice.Speak(Number1.ToString & GetActiveTypesOperatorName & Number2 & " equals " & value.ToString.Replace("-", "negative "))
+            If value = SelectedValue Then
+                MarkXO(True, intValue)
+            Else
+                MarkXO(False, intValue)
+            End If
+        Else
+            voice.SpeakAsync("Oops!. Enter the value in the textbox, and try again.")
+        End If
+    End Sub
+
+#End Region
+
+#Region "Supporting Methods"
 
     Private Sub PopulateField()
         Me.Text = "TicTac" & ActiveGameType.ToString()
@@ -42,51 +94,13 @@ Public Class Form1
             Dim Number1 As Integer = rand.Next(1, Max)
             Dim Number2 As Integer = rand.Next(1, Max)
             If Not allowNegatives AndAlso Number1 < Number2 Then
-                L.Text = String.Concat(Number2.ToString, Environment.NewLine, GameOperator(ActiveGameType), Number1.ToString)
+                L.Text = String.Concat(Number2.ToString, Environment.NewLine, GameOperator, Number1.ToString)
             Else
-                L.Text = String.Concat(Number1.ToString, Environment.NewLine, GameOperator(ActiveGameType), Number2.ToString)
+                L.Text = String.Concat(Number1.ToString, Environment.NewLine, GameOperator, Number2.ToString)
             End If
             L.Visible = True
         Next
         Button1.Visible = True : Button2.Visible = True : Button3.Visible = True : Button4.Visible = True : Button5.Visible = True : Button6.Visible = True : Button7.Visible = True : Button8.Visible = True : Button9.Visible = True
-    End Sub
-
-    Private Function GameOperator(gt As GameType) As Char
-        Select Case gt
-            Case GameType.Multiply
-                Return "x"c
-            Case GameType.Add
-                Return "+"c
-            Case Else
-                Return "-"c
-        End Select
-    End Function
-
-
-    Private RadioNumberSelected As Integer = 1
-
-
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
-        Dim ob As Button = DirectCast(sender, Button)
-        Dim intValue As Integer = ButtonArray.IndexOf(ob) + 1 'Integer.Parse(ob.Name.Substring(6))
-        Dim SelectedValue As Integer
-        'Dim LabelValueToCheck As String = LabelArray(intValue - 1).Text
-        If Not String.IsNullOrWhiteSpace(TextboxArray(intValue - 1).Text) AndAlso Integer.TryParse(TextboxArray(intValue - 1).Text, SelectedValue) Then
-            ob.Visible = False
-
-            Dim Number1, Number2 As Integer
-            ParseNumbers(LabelArray(intValue - 1).Text, Number1, Number2)
-            Dim value As Integer = CalculateValue(Number1, Number2)
-            voice.Speak(Number1.ToString & GetActiveTypesOperatorName & Number2 & " equals " & value.ToString.Replace("-", "negative "))
-            If value = SelectedValue Then
-                MarkXO(True, intValue)
-            Else
-                MarkXO(False, intValue)
-            End If
-        Else
-            voice.SpeakAsync("Oops!. Enter the value in the textbox, and try again.")
-        End If
     End Sub
 
     Private Function CalculateValue(number1 As Integer, number2 As Integer)
@@ -99,20 +113,6 @@ Public Class Form1
                 Return number1 - number2
         End Select
     End Function
-
-    Private ReadOnly Property GetActiveTypesOperatorName As String
-        Get
-            Select Case ActiveGameType
-                Case GameType.Add
-                    Return " plus "
-                Case GameType.Multiply
-                    Return " times "
-                Case Else
-                    Return " minus "
-            End Select
-        End Get
-    End Property
-
 
     Private Sub MarkXO(Player As Boolean, ArrayID As Integer)
         Const PlayerWon As String = "You won!"
@@ -137,8 +137,6 @@ Public Class Form1
                 Win(ComputerWon)
             End If
         End If
-
-
     End Sub
 
     Private Sub ComputersTurn()
@@ -154,11 +152,7 @@ Public Class Form1
         ParseNumbers(TextToParse, Number1, Number2)
         voice.SpeakAsync("My Pick. " & Number1.ToString & GetActiveTypesOperatorName & Number2 & " equals " & CalculateValue(Number1, Number2).ToString.Replace("-", "negative "))
         LabelArray(I).Text = "O" : LabelArray(I).ForeColor = Color.Red
-
-
     End Sub
-
-
 
     Private Sub ParseNumbers(S As String, ByRef Number1 As Integer, ByRef Number2 As Integer)
         Dim Numbers() As String = S.Split(Environment.NewLine & "-")
@@ -172,9 +166,6 @@ Public Class Form1
         PopulateField()
     End Sub
 
-    Private Player1Values As New Generic.List(Of Integer)()
-    Private ComputerValues As New Generic.List(Of Integer)()
-
     Private Function CheckForWin(Player As Generic.List(Of Integer)) As Boolean
         If Player.Contains(1) AndAlso Player.Contains(2) AndAlso Player.Contains(3) Then Return True
         If Player.Contains(1) AndAlso Player.Contains(4) AndAlso Player.Contains(7) Then Return True
@@ -187,6 +178,9 @@ Public Class Form1
         Return False
     End Function
 
+#End Region
+
+#Region "Options Event Handles"
     Private Sub YesNoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles YesToolStripMenuItem.Click, NoToolStripMenuItem.Click
         DirectCast(sender, ToolStripMenuItem).Checked = True
         If DirectCast(sender, ToolStripMenuItem) Is YesToolStripMenuItem Then
@@ -216,14 +210,6 @@ Public Class Form1
         PopulateField()
     End Sub
 
-    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
-        voice.Dispose()
-    End Sub
-
-    Private Sub ExitToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem1.Click
-        Me.Close()
-    End Sub
-
     Private Sub SubtractionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SubtractionToolStripMenuItem.Click, AdditionToolStripMenuItem.Click, MultiplicationToolStripMenuItem.Click
         Dim castSender As ToolStripMenuItem = DirectCast(sender, ToolStripMenuItem)
         Dim arrayList As New Generic.LinkedList(Of ToolStripMenuItem)({SubtractionToolStripMenuItem, AdditionToolStripMenuItem, MultiplicationToolStripMenuItem})
@@ -235,5 +221,26 @@ Public Class Form1
         Next
         PopulateField()
     End Sub
+#End Region
+
+#Region "Form Events"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        voice.SelectVoiceByHints(Speech.Synthesis.VoiceGender.Female)
+        voice.SpeakAsync("Hello, I am ready to play some Tic Tac Math with you today. You start first.")
+
+        LabelArray.AddRange({Label1, Label2, Label3, Label4, Label5, Label6, Label7, Label8, Label9})
+        TextboxArray.AddRange({TextBox1, TextBox2, TextBox3, TextBox4, TextBox5, TextBox6, TextBox7, TextBox8, TextBox9})
+        ButtonArray.AddRange({Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9})
+        PopulateField()
+    End Sub
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        voice.Dispose()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem1.Click
+        Me.Close()
+    End Sub
+#End Region
 
 End Class
--- TicTacSubtrack/My Project/AssemblyInfo.vb
+++ TicTacSubtrack/My Project/AssemblyInfo.vb
@@ -1,4 +1,5 @@
-﻿Imports System
+﻿Imports System.Resources
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -10,7 +11,7 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("TicTacSubtrack")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyCompany("Luke Liukonen")>
 <Assembly: AssemblyProduct("TicTacSubtrack")>
 <Assembly: AssemblyCopyright("Copyright ©  2016")>
 <Assembly: AssemblyTrademark("")>
@@ -33,3 +34,4 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
